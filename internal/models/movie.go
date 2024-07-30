367c2d20fd52d8948abc943476411e1d5c792ec7
hixi4 <167585616+hixi4@users.noreply.github.com>
2024-07-30 21:37:20 +0300

    Update movie.go

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -1,1 +1,19 @@
+package models
 
+import "time"
+
+// Movie структура для зберігання інформації про фільм
+type Movie struct {
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Transport   string  `json:"transport"`
+}
+
+// Order структура для зберігання інформації про замовлення
+type Order struct {
+	ID         string    `json:"id"`
+	MovieTitle string    `json:"movie_title"`
+	Status     string    `json:"status"`
+	OrderedAt  time.Time `json:"ordered_at"`
+}
