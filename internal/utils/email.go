c02715ff5aeee94b3cb837fdcb1b94366c0c1d9e
hixi4 <167585616+hixi4@users.noreply.github.com>
2024-07-30 21:39:24 +0300

    Update email.go

--- internal/utils/email.go
+++ internal/utils/email.go
@@ -1,1 +1,10 @@
+package utils
+
+import "log"
+
+// SendEmail симулює відправку email
+func SendEmail(orderID string) {
+	// Для реальної реалізації замініть цю функцію на код для відправки email
+	log.Printf("Email sent for order ID: %s", orderID)
+}
 
