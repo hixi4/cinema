06987bf8a3becda1e881f5564bf41b7954d6c222
hixi4 <167585616+hixi4@users.noreply.github.com>
2024-08-01 00:51:22 +0300

    Update movie_repostory.go

--- internal/repository/movie_repostory.go
+++ internal/repository/movie_repostory.go
@@ -1,38 +1,39 @@
-package repositories
+package repository
 
 import (
-	"cinema/internal/models"
+	"cinema/internal/model"
 )
 
 // MovieRepository структура для репозиторію фільмів
 type MovieRepository struct {
-	movies []models.Movie
-	orders []models.Order
+	movies []model.Movie
+	orders []model.Order
 }
 
 // NewMovieRepository створює новий екземпляр MovieRepository
 func NewMovieRepository() *MovieRepository {
 	return &MovieRepository{
-		movies: []models.Movie{
+		movies: []model.Movie{
 			{Title: "Movie 1", Description: "Description 1", Price: 10.0, Transport: "Bus"},
 			{Title: "Movie 2", Description: "Description 2", Price: 12.0, Transport: "Train"},
 		},
-		orders: make([]models.Order, 0),
+		orders: make([]model.Order, 0),
 	}
 }
 
 // GetAvailableMovies повертає список доступних фільмів
-func (r *MovieRepository) GetAvailableMovies() []models.Movie {
+func (r *MovieRepository) GetAvailableMovies() []model.Movie {
 	return r.movies
 }
 
 // PlaceOrder створює нове замовлення
-func (r *MovieRepository) PlaceOrder(order models.Order) {
+func (r *MovieRepository) PlaceOrder(order model.Order) {
 	r.orders = append(r.orders, order)
 }
 
 // GetOrders повертає список замовлених фільмів
-func (r *MovieRepository) GetOrders() []models.Order {
+func (r *MovieRepository) GetOrders() []model.Order {
 	return r.orders
 }
+
 
